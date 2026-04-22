b706b72b32e0f1cbfbaa302e6a387699259a5e6b
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:36:52 -0500

    Upload via GitDash

--- monaco.contribution.js
+++ monaco.contribution.js
@@ -1,11 +1,6 @@
-define("vs/language/html/monaco.contribution", ["exports", "../../editor.api-CykLys8L", "../../monaco.contribution-D74vF5SR"], (function(exports, editor_api, language_html_monaco_contribution) {
+define("vs/language/json/monaco.contribution", ["exports", "../../editor.api-CykLys8L", "../../monaco.contribution-Cqdn_4Kw"], (function(exports, editor_api, language_json_monaco_contribution) {
   "use strict";
-  exports.handlebarDefaults = language_html_monaco_contribution.handlebarDefaults;
-  exports.handlebarLanguageService = language_html_monaco_contribution.handlebarLanguageService;
-  exports.htmlDefaults = language_html_monaco_contribution.htmlDefaults;
-  exports.htmlLanguageService = language_html_monaco_contribution.htmlLanguageService;
-  exports.razorDefaults = language_html_monaco_contribution.razorDefaults;
-  exports.razorLanguageService = language_html_monaco_contribution.razorLanguageService;
-  exports.registerHTMLLanguageService = language_html_monaco_contribution.registerHTMLLanguageService;
+  exports.getWorker = language_json_monaco_contribution.getWorker;
+  exports.jsonDefaults = language_json_monaco_contribution.jsonDefaults;
   Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
 }));
