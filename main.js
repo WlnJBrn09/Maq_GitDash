82ea2fbfa178b84b7c17c12652d780ffce8b4f9c
Waylon Brown <waylonjacebrown@gmail.com>
2026-04-22 07:37:42 -0500

    Upload via GitDash

--- main.js
+++ main.js
@@ -1,278 +1,93 @@
-#!/usr/bin/env node
+import { format as format$1 } from './impl/format.js';
+import { parse as parse$1, getNodePath as getNodePath$1, getNodeValue as getNodeValue$1, findNodeAtOffset as findNodeAtOffset$1 } from './impl/parser.js';
+import { createScanner as createScanner$1 } from './impl/scanner.js';
 
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 /**
- * Marked CLI
- * Copyright (c) 2011-2013, Christopher Jeffrey (MIT License)
+ * Creates a JSON scanner on the given text.
+ * If ignoreTrivia is set, whitespaces or comments are ignored.
  */
-
-import { promises } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { homedir } from 'node:os';
-import { createRequire } from 'node:module';
-import { marked } from '../lib/marked.esm.js';
-
-const { access, readFile, writeFile } = promises;
-const require = createRequire(import.meta.url);
-
+const createScanner = createScanner$1;
+var ScanError;
+(function (ScanError) {
+    ScanError[ScanError["None"] = 0] = "None";
+    ScanError[ScanError["UnexpectedEndOfComment"] = 1] = "UnexpectedEndOfComment";
+    ScanError[ScanError["UnexpectedEndOfString"] = 2] = "UnexpectedEndOfString";
+    ScanError[ScanError["UnexpectedEndOfNumber"] = 3] = "UnexpectedEndOfNumber";
+    ScanError[ScanError["InvalidUnicode"] = 4] = "InvalidUnicode";
+    ScanError[ScanError["InvalidEscapeCharacter"] = 5] = "InvalidEscapeCharacter";
+    ScanError[ScanError["InvalidCharacter"] = 6] = "InvalidCharacter";
+})(ScanError || (ScanError = {}));
+var SyntaxKind;
+(function (SyntaxKind) {
+    SyntaxKind[SyntaxKind["OpenBraceToken"] = 1] = "OpenBraceToken";
+    SyntaxKind[SyntaxKind["CloseBraceToken"] = 2] = "CloseBraceToken";
+    SyntaxKind[SyntaxKind["OpenBracketToken"] = 3] = "OpenBracketToken";
+    SyntaxKind[SyntaxKind["CloseBracketToken"] = 4] = "CloseBracketToken";
+    SyntaxKind[SyntaxKind["CommaToken"] = 5] = "CommaToken";
+    SyntaxKind[SyntaxKind["ColonToken"] = 6] = "ColonToken";
+    SyntaxKind[SyntaxKind["NullKeyword"] = 7] = "NullKeyword";
+    SyntaxKind[SyntaxKind["TrueKeyword"] = 8] = "TrueKeyword";
+    SyntaxKind[SyntaxKind["FalseKeyword"] = 9] = "FalseKeyword";
+    SyntaxKind[SyntaxKind["StringLiteral"] = 10] = "StringLiteral";
+    SyntaxKind[SyntaxKind["NumericLiteral"] = 11] = "NumericLiteral";
+    SyntaxKind[SyntaxKind["LineCommentTrivia"] = 12] = "LineCommentTrivia";
+    SyntaxKind[SyntaxKind["BlockCommentTrivia"] = 13] = "BlockCommentTrivia";
+    SyntaxKind[SyntaxKind["LineBreakTrivia"] = 14] = "LineBreakTrivia";
+    SyntaxKind[SyntaxKind["Trivia"] = 15] = "Trivia";
+    SyntaxKind[SyntaxKind["Unknown"] = 16] = "Unknown";
+    SyntaxKind[SyntaxKind["EOF"] = 17] = "EOF";
+})(SyntaxKind || (SyntaxKind = {}));
 /**
- * @param {Process} nodeProcess inject process so it can be mocked in tests.
+ * Parses the given text and returns the object the JSON content represents. On invalid input, the parser tries to be as fault tolerant as possible, but still return a result.
+ * Therefore, always check the errors list to find out if the input was valid.
  */
-export async function main(nodeProcess) {
-  /**
-   * Man Page
-   */
-  async function help() {
-    const { spawn } = await import('child_process');
-    const { fileURLToPath } = await import('url');
-
-    const options = {
-      cwd: nodeProcess.cwd(),
-      env: nodeProcess.env,
-      stdio: 'inherit',
-    };
-
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    const helpText = await readFile(resolve(__dirname, '../man/marked.1.md'), 'utf8');
-
-    await new Promise(res => {
-      spawn('man', [resolve(__dirname, '../man/marked.1')], options)
-        .on('error', () => {
-          console.log(helpText);
-        })
-        .on('close', res);
-    });
-  }
-
-  async function version() {
-    const pkg = require('../package.json');
-    console.log(pkg.version);
-  }
-
-  /**
-   * Main
-   */
-  async function start(argv) {
-    const files = [];
-    const options = {};
-    let input;
-    let output;
-    let string;
-    let arg;
-    let tokens;
-    let config;
-    let opt;
-    let noclobber;
-
-    function getArg() {
-      let arg = argv.shift();
-
-      if (arg.indexOf('--') === 0) {
-        // e.g. --opt
-        arg = arg.split('=');
-        if (arg.length > 1) {
-          // e.g. --opt=val
-          argv.unshift(arg.slice(1).join('='));
-        }
-        arg = arg[0];
-      } else if (arg[0] === '-') {
-        if (arg.length > 2) {
-          // e.g. -abc
-          argv = arg.substring(1).split('').map(function(ch) {
-            return '-' + ch;
-          }).concat(argv);
-          arg = argv.shift();
-        } else {
-          // e.g. -a
-        }
-      } else {
-        // e.g. foo
-      }
-
-      return arg;
-    }
-
-    while (argv.length) {
-      arg = getArg();
-      switch (arg) {
-        case '-o':
-        case '--output':
-          output = argv.shift();
-          break;
-        case '-i':
-        case '--input':
-          input = argv.shift();
-          break;
-        case '-s':
-        case '--string':
-          string = argv.shift();
-          break;
-        case '-t':
-        case '--tokens':
-          tokens = true;
-          break;
-        case '-c':
-        case '--config':
-          config = argv.shift();
-          break;
-        case '-n':
-        case '--no-clobber':
-          noclobber = true;
-          break;
-        case '-h':
-        case '--help':
-          return await help();
-        case '-v':
-        case '--version':
-          return await version();
-        default:
-          if (arg.indexOf('--') === 0) {
-            opt = camelize(arg.replace(/^--(no-)?/, ''));
-            if (!(opt in marked.defaults)) {
-              continue;
-            }
-            if (arg.indexOf('--no-') === 0) {
-              options[opt] = typeof marked.defaults[opt] !== 'boolean'
-                ? null
-                : false;
-            } else {
-              options[opt] = typeof marked.defaults[opt] !== 'boolean'
-                ? argv.shift()
-                : true;
-            }
-          } else {
-            files.push(arg);
-          }
-          break;
-      }
-    }
-
-    async function getData() {
-      if (!input) {
-        if (files.length <= 2) {
-          if (string) {
-            return string;
-          }
-          return await getStdin();
-        }
-        input = files.pop();
-      }
-      return await readFile(input, 'utf8');
-    }
-
-    function resolveFile(file) {
-      return resolve(file.replace(/^~/, homedir));
-    }
-
-    function fileExists(file) {
-      return access(resolveFile(file)).then(() => true, () => false);
-    }
-
-    async function runConfig(file) {
-      const configFile = resolveFile(file);
-      let markedConfig;
-      try {
-        // try require for json
-        markedConfig = require(configFile);
-      } catch (err) {
-        if (err.code !== 'ERR_REQUIRE_ESM') {
-          throw err;
-        }
-        // must import esm
-        markedConfig = await import('file:///' + configFile);
-      }
-
-      if (markedConfig.default) {
-        markedConfig = markedConfig.default;
-      }
-
-      if (typeof markedConfig === 'function') {
-        markedConfig(marked);
-      } else {
-        marked.use(markedConfig);
-      }
-    }
-
-    const data = await getData();
-
-    if (config) {
-      if (!await fileExists(config)) {
-        throw Error(`Cannot load config file '${config}'`);
-      }
-
-      await runConfig(config);
-    } else {
-      const defaultConfig = [
-        '~/.marked.json',
-        '~/.marked.js',
-        '~/.marked/index.js',
-      ];
-
-      for (const configFile of defaultConfig) {
-        if (await fileExists(configFile)) {
-          await runConfig(configFile);
-          break;
-        }
-      }
-    }
-
-    const html = tokens
-      ? JSON.stringify(marked.lexer(data, options), null, 2)
-      : await marked.parse(data, options);
-
-    if (output) {
-      if (noclobber && await fileExists(output)) {
-        throw Error('marked: output file \'' + output + '\' already exists, disable the \'-n\' / \'--no-clobber\' flag to overwrite\n');
-      }
-      return await writeFile(output, html);
-    }
-
-    nodeProcess.stdout.write(html + '\n');
-  }
-
-  /**
-   * Helpers
-   */
-  function getStdin() {
-    return new Promise((resolve, reject) => {
-      const stdin = nodeProcess.stdin;
-      let buff = '';
-
-      stdin.setEncoding('utf8');
-
-      stdin.on('data', function(data) {
-        buff += data;
-      });
-
-      stdin.on('error', function(err) {
-        reject(err);
-      });
-
-      stdin.on('end', function() {
-        resolve(buff);
-      });
-
-      stdin.resume();
-    });
-  }
-
-  /**
-   * @param {string} text
-   */
-  function camelize(text) {
-    return text.replace(/(\w)-(\w)/g, function(_, a, b) {
-      return a + b.toUpperCase();
-    });
-  }
-
-  try {
-    await start(nodeProcess.argv.slice());
-    nodeProcess.exit(0);
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      nodeProcess.stderr.write('marked: ' + err.path + ': No such file or directory');
-    } else {
-      nodeProcess.stderr.write(err.message);
-    }
-    return nodeProcess.exit(1);
-  }
+const parse = parse$1;
+/**
+ * Finds the innermost node at the given offset. If includeRightBound is set, also finds nodes that end at the given offset.
+ */
+const findNodeAtOffset = findNodeAtOffset$1;
+/**
+ * Gets the JSON path of the given JSON DOM node
+ */
+const getNodePath = getNodePath$1;
+/**
+ * Evaluates the JavaScript object of the given JSON DOM node
+ */
+const getNodeValue = getNodeValue$1;
+var ParseErrorCode;
+(function (ParseErrorCode) {
+    ParseErrorCode[ParseErrorCode["InvalidSymbol"] = 1] = "InvalidSymbol";
+    ParseErrorCode[ParseErrorCode["InvalidNumberFormat"] = 2] = "InvalidNumberFormat";
+    ParseErrorCode[ParseErrorCode["PropertyNameExpected"] = 3] = "PropertyNameExpected";
+    ParseErrorCode[ParseErrorCode["ValueExpected"] = 4] = "ValueExpected";
+    ParseErrorCode[ParseErrorCode["ColonExpected"] = 5] = "ColonExpected";
+    ParseErrorCode[ParseErrorCode["CommaExpected"] = 6] = "CommaExpected";
+    ParseErrorCode[ParseErrorCode["CloseBraceExpected"] = 7] = "CloseBraceExpected";
+    ParseErrorCode[ParseErrorCode["CloseBracketExpected"] = 8] = "CloseBracketExpected";
+    ParseErrorCode[ParseErrorCode["EndOfFileExpected"] = 9] = "EndOfFileExpected";
+    ParseErrorCode[ParseErrorCode["InvalidCommentToken"] = 10] = "InvalidCommentToken";
+    ParseErrorCode[ParseErrorCode["UnexpectedEndOfComment"] = 11] = "UnexpectedEndOfComment";
+    ParseErrorCode[ParseErrorCode["UnexpectedEndOfString"] = 12] = "UnexpectedEndOfString";
+    ParseErrorCode[ParseErrorCode["UnexpectedEndOfNumber"] = 13] = "UnexpectedEndOfNumber";
+    ParseErrorCode[ParseErrorCode["InvalidUnicode"] = 14] = "InvalidUnicode";
+    ParseErrorCode[ParseErrorCode["InvalidEscapeCharacter"] = 15] = "InvalidEscapeCharacter";
+    ParseErrorCode[ParseErrorCode["InvalidCharacter"] = 16] = "InvalidCharacter";
+})(ParseErrorCode || (ParseErrorCode = {}));
+/**
+ * Computes the edit operations needed to format a JSON document.
+ *
+ * @param documentText The input text
+ * @param range The range to format or `undefined` to format the full content
+ * @param options The formatting options
+ * @returns The edit operations describing the formatting changes to the original document following the format described in {@linkcode EditResult}.
+ * To apply the edit operations to the input, use {@linkcode applyEdits}.
+ */
+function format(documentText, range, options) {
+    return format$1(documentText, range, options);
 }
+
+export { ParseErrorCode, ScanError, SyntaxKind, createScanner, findNodeAtOffset, format, getNodePath, getNodeValue, parse };
